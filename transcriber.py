dfdeb829dc8e72f7e6a101f1fdc2ee570b311d50
massimocristi1970 <massimo@ticktockloans.com>
2025-06-05 15:02:19 +0100

    Update transcriber.py

--- transcriber.py
+++ transcriber.py
@@ -1,8 +1,9 @@
 # transcriber.py
 import whisper
 
-model = whisper.load_model("base")  # Options: "tiny", "base", "small", "medium", "large"
+model = whisper.load_model("small")  # Options: "tiny", "base", "small", "medium", "large"
 
 def transcribe_audio(file_path):
-    result = model.transcribe(file_path)
-    return result['text']
+    result = model.transcribe(file_path, fp16=False)
+    return result["text"]
+
